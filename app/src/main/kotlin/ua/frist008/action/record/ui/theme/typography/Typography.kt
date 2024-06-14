aa2534615f329a53541379d5df89f23119f9e905
Konstiantyn Rudoi <frist008@gmail.com>
2024-06-15 02:02:48 +0300

    add error screen when devices not found

--- app/src/main/kotlin/ua/frist008/action/record/ui/theme/typography/Typography.kt
+++ app/src/main/kotlin/ua/frist008/action/record/ui/theme/typography/Typography.kt
@@ -2,24 +2,73 @@ package ua.frist008.action.record.ui.theme.typography
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.sp
+import ua.frist008.action.record.ui.theme.color.AppColorScheme
+import ua.frist008.action.record.ui.theme.color.Palette
 
 private val LocalTypography = Typography()
 
-val DisplayLargeStyle: TextStyle = LocalTypography.displayLarge
-val DisplayMediumStyle: TextStyle = LocalTypography.displayMedium
-val DisplaySmallStyle: TextStyle = LocalTypography.displaySmall
-val HeadlineLargeStyle: TextStyle = LocalTypography.headlineLarge
-val HeadlineMediumStyle: TextStyle = LocalTypography.headlineMedium
-val HeadlineSmallStyle: TextStyle = LocalTypography.headlineSmall
-val TitleLargeStyle: TextStyle = LocalTypography.titleLarge
-val TitleMediumStyle: TextStyle = LocalTypography.titleMedium
-val TitleSmallStyle: TextStyle = LocalTypography.titleSmall
-val BodyLargeStyle: TextStyle = LocalTypography.bodyLarge
-val BodyMediumStyle: TextStyle = LocalTypography.bodyMedium
-val BodySmallStyle: TextStyle = LocalTypography.bodySmall
-val LabelLargeStyle: TextStyle = LocalTypography.labelLarge
-val LabelMediumStyle: TextStyle = LocalTypography.labelMedium
-val LabelSmallStyle: TextStyle = LocalTypography.labelSmall
+// Display
+val DisplayLargeStyle: TextStyle = LocalTypography.displayLarge.copy(
+    color = AppColorScheme.onBackground,
+)
+val DisplayMediumStyle: TextStyle = LocalTypography.displayMedium.copy(
+    color = AppColorScheme.onBackground,
+)
+val DisplaySmallStyle: TextStyle = LocalTypography.displaySmall.copy(
+    color = AppColorScheme.onBackground,
+)
+
+// Headline
+val HeadlineLargeStyle: TextStyle = LocalTypography.headlineLarge.copy(
+    color = AppColorScheme.onBackground,
+)
+val HeadlineMediumStyle: TextStyle = LocalTypography.headlineMedium.copy(
+    color = AppColorScheme.onBackground,
+)
+val HeadlineSmallStyle: TextStyle = LocalTypography.headlineSmall.copy(
+    color = AppColorScheme.onBackground,
+    fontSize = 22.sp,
+    fontWeight = FontWeight.Bold,
+    lineHeight = 26.sp,
+)
+
+// Title
+val TitleLargeStyle: TextStyle = LocalTypography.titleLarge.copy(
+    color = AppColorScheme.onBackground,
+    fontSize = 22.sp,
+)
+val TitleMediumStyle: TextStyle = LocalTypography.titleMedium.copy(
+    color = AppColorScheme.onBackground,
+)
+val TitleSmallStyle: TextStyle = LocalTypography.titleSmall.copy(
+    color = AppColorScheme.onBackground,
+)
+
+// Body
+val BodyLargeStyle: TextStyle = LocalTypography.bodyLarge.copy(
+    color = AppColorScheme.onBackground,
+)
+val BodyMediumStyle: TextStyle = LocalTypography.bodyMedium.copy(
+    color = AppColorScheme.onBackground,
+    fontSize = 14.sp,
+)
+val BodySmallStyle: TextStyle = LocalTypography.bodySmall.copy(
+    color = AppColorScheme.onBackground,
+)
+
+// Label
+val LabelLargeStyle: TextStyle = LocalTypography.labelLarge.copy(
+    color = AppColorScheme.onBackground,
+)
+val LabelMediumStyle: TextStyle = LocalTypography.labelMedium.copy(
+    color = AppColorScheme.onBackground,
+)
+val LabelSmallStyle: TextStyle = LocalTypography.labelSmall.copy(
+    color = AppColorScheme.onBackground,
+)
 
 val Typography = Typography(
     displayLarge = DisplayLargeStyle,
@@ -38,3 +87,10 @@ val Typography = Typography(
     labelMedium = LabelMediumStyle,
     labelSmall = LabelSmallStyle,
 )
+
+val Typography.link: TextStyle by lazy(LazyThreadSafetyMode.NONE) {
+    Typography.bodyMedium.merge(
+        color = Palette.BLUE_LIGHT,
+        textDecoration = TextDecoration.Underline,
+    )
+}
